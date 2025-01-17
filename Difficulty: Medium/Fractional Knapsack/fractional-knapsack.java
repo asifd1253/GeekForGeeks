06710ebd9bf1f18e4063e4aab9bebb43b1a25bf2
Asif <140490107+asifd1253@users.noreply.github.com>
2025-01-17 12:43:19 +0530

    Update fractional-knapsack.java

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.java
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.java
@@ -1,65 +1,27 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GfG {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-
-        while (t-- > 0) {
-            // Read values array
-            String[] valueInput = br.readLine().trim().split(" ");
-            List<Integer> values = new ArrayList<>();
-            for (String s : valueInput) {
-                values.add(Integer.parseInt(s));
-            }
-
-            // Read weights array
-            String[] weightInput = br.readLine().trim().split(" ");
-            List<Integer> weights = new ArrayList<>();
-            for (String s : weightInput) {
-                weights.add(Integer.parseInt(s));
-            }
-
-            // Read the knapsack capacity
-            int w = Integer.parseInt(br.readLine().trim());
-
-            // Call fractionalKnapsack function and print result
-            System.out.println(String.format(
-                "%.6f", new Solution().fractionalKnapsack(values, weights, w)));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 class Solution {
     // Function to get the maximum total value in the knapsack.
     double fractionalKnapsack(List<Integer> val, List<Integer> wt, int capacity) {
         // code here
-        ArrayList<int[]> ratio = new ArrayList<>();
+        ArrayList<double[]> ratio = new ArrayList<>();
         
         for(int i=0; i<val.size(); i++){
             double rat = (double)val.get(i)/wt.get(i);
-            ratio.add(new int[]{i,(int)(rat*100000)});
+            ratio.add(new double[]{i,rat});
         }
         
-        ratio.sort((a,b)-> b[1] - a[1]);
+        ratio.sort((a,b)-> Double.compare(b[1], a[1]));
         
         double result = 0;
-        for(int[] item : ratio){
-            int index = item[0];
+        for(double[] item : ratio){
+            int index = (int)item[0];
             
             if(capacity >= wt.get(index)){
                 capacity -= wt.get(index);
                 result = result + val.get(index);
             }else{
-                result = result + ((double) val.get(index)/wt.get(index)) * capacity;
+                // If only a fraction of the item can fit, take that fraction.
+                result = result + (item[1]*capacity);
                 break;
             }
         }
