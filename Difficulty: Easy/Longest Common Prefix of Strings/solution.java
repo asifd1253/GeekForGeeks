198517cdf18da39dd0af4ff0881f233d68fc0624
Asif <140490107+asifd1253@users.noreply.github.com>
2025-01-24 23:09:02 +0530

    Create solution.java

---
+++ Difficulty: Easy/Longest Common Prefix of Strings/solution.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+class Solution {
+    public String longestCommonPrefix(String arr[]) {
+        // code here
+        if(arr == null || arr.length == 0){
+            return "";
+        }
+        
+        String prefix = arr[0];
+        
+        for(int i=1; i<arr.length; i++){
+            while(!arr[i].startsWith(prefix)){
+                prefix = prefix.substring(0,prefix.length()-1);
+                if(prefix.length() == 0){
+                    return "";
+                }
+            }
+        }
+        
+        return prefix;
+    }
+}
