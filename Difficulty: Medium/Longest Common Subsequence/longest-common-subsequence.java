5db781bbeabca49965519c9605f9ee3a83a2443e
Asif <140490107+asifd1253@users.noreply.github.com>
2025-01-30 10:37:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.java
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.java
@@ -27,37 +27,30 @@ class GFG {
 
 
 class Solution {
-    public static int helper(String s1, String s2, int n, int m, int dp[][]){
-        if(n == 0 || m == 0){
-            return 0;
-        }
-        
-        if(dp[n][m] != -1){
-            return dp[n][m];
-        }
-        
-        //case- 1:
-                    //-1 because index 0
-        if(s1.charAt(n-1) == s2.charAt(m-1)){
-            dp[n][m] = helper(s1, s2, n-1, m-1,dp)+1;
-        }else{   //diff 
-            int ans1 = helper(s1, s2, n, m-1, dp);
-            int ans2 = helper(s1, s2, n-1, m, dp);
-            dp[n][m] = Math.max(ans1, ans2);
-        }
-        
-        return dp[n][m];
-    }
     // Function to find the length of the longest common subsequence in two strings.
     static int lcs(String s1, String s2) {
         // your code here
         int dp[][] = new int[s1.length()+1][s2.length()+1];
-        for(int i=0; i<s1.length()+1; i++){
-            for(int j=0; j<s2.length()+1; j++){
-                dp[i][j] = -1;
+        
+        for(int i=0; i<dp[0].length; i++){
+            dp[0][i] = 0;
+        }
+        for(int i=0; i<dp.length; i++){
+            dp[i][0] = 0;
+        }
+        
+        for(int i=1; i<s1.length()+1; i++){
+            for(int j=1; j<s2.length()+1; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else{
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
             }
         }
         
-        return helper(s1, s2, s1.length(), s2.length(), dp);
+        return dp[s1.length()][s2.length()];
     }
 }
