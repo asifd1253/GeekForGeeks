d3bd87aff81358861a5c4be46b4ed94714845507
Asif <140490107+asifd1253@users.noreply.github.com>
2025-02-13 08:39:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nth Catalan Number/nth-catalan-number.java
+++ Difficulty: Medium/Nth Catalan Number/nth-catalan-number.java
@@ -27,28 +27,19 @@ class GFG {
 
 
 class Solution {
-    public static int catalMemo(int n, int dp[]){
-        if(n==0 || n==1){
-            return 1;
-        }
-        
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        
-        dp[n] = 0;
-        
-        for(int i=0; i<n; i++){
-            dp[n] += catalMemo(i, dp) * catalMemo(n-1-i, dp);
-        }
-        
-        return dp[n];
-    }
     public static int findCatalan(int n) {
         // code here
         int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        dp[1] = 1;
         
-        return catalMemo(n, dp);
+        for(int i=2; i<n+1; i++){
+            for(int j=0; j<i; j++){
+                dp[i] += dp[j]*dp[i-1-j];
+            }
+        }
+        
+        
+        return dp[n];
     }
 }
