8dcf3e7bd0e921dd736f2477c946b19b7eb114a9
Asif <140490107+asifd1253@users.noreply.github.com>
2025-02-13 08:42:14 +0530

    Create Tabulation.java

---
+++ Difficulty: Medium/Nth Catalan Number/Tabulation.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int findCatalan(int n) {
+        // code here
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2; i<n+1; i++){
+            for(int j=0; j<i; j++){
+                dp[i] += dp[j]*dp[i-1-j];
+            }
+        }
+        
+        
+        return dp[n];
+    }
+}
