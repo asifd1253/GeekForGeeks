e49a9167daf056e2ed7bdb3a7163389af3763cbd
Asif <140490107+asifd1253@users.noreply.github.com>
2025-02-19 07:19:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Unique BST's/unique-bsts.java
+++ Difficulty: Medium/Unique BST's/unique-bsts.java
@@ -19,7 +19,9 @@ class Solution {
         
         for(int i=2; i<n+1; i++){
             for(int j=0; j<i; j++){
-                dp[i]  += dp[j] * dp[i-1-j];
+                int leftSubTrees = dp[j];
+                int rightSubTrees = dp[i-1-j];
+                dp[i]  += leftSubTrees * rightSubTrees;
             }
         }
         
