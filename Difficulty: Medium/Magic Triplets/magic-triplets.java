c88dd32a8160ab1e89fe101d07a01b018fe61fb6
Asif <140490107+asifd1253@users.noreply.github.com>
2025-06-28 11:03:25 +0530

    Update magic-triplets.java

--- Difficulty: Medium/Magic Triplets/magic-triplets.java
+++ Difficulty: Medium/Magic Triplets/magic-triplets.java
@@ -1,62 +1,30 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class GFG
-{
-    public static void main(String[] args) throws IOException
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while(T-->0)
-        {
-            int n = Integer.parseInt(br.readLine().trim());
-            String s = br.readLine().trim();
-            String[] S = s.split(" ");
-            int[] nums = new int[n];
-            for(int i = 0; i < n; i++){
-                nums[i] = Integer.parseInt(S[i]);
-            }
-            Solution ob = new Solution();
-            int ans = ob.countTriplets(nums);
-            System.out.println(ans);         
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-class Solution{
-    public int countTriplets(int[] arr){
+class Solution {
+    public int countTriplets(int[] arr) {
         // code here
-        int count = 0;
         int n = arr.length;
-        int leftSmall[] = new int[n];
-        int rightGreat[] = new int[n];
+        
+        int count = 0;
+        int leftSmallEle[] = new int[n];
+        int rightGreatEle[] = new int[n];
         
         for(int i=0; i<n; i++){
-            int curr = arr[i];
-            for(int j=0; j<i; j++){  //To iterate on left side of a value
-                if(arr[j] < curr){
-                    leftSmall[i]++;
+            int currEle = arr[i];
+            
+            for(int j=0; j<i; j++){     //To iterate on left side of a value
+                if(currEle > arr[j]){
+                    leftSmallEle[i]++;  // Increment inside array element
                 }
             }
-            for(int j=i; j<n; j++){  //To iterate on right side of a value
-                if(arr[j] > curr){
-                    rightGreat[i]++;
+            
+            for(int k=i; k<n; k++){
+                if(currEle < arr[k]){
+                    rightGreatEle[i]++;
                 }
             }
         }
         
-        for(int i=0; i<n; i++){
-            count += leftSmall[i]*rightGreat[i];
+        for(int m=0; m<n; m++){
+            count = count + (leftSmallEle[m] * rightGreatEle[m]);
         }
         
         return count;
