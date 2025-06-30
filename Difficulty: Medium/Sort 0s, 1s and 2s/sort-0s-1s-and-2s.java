377f4116085afec73e263d12e6b153eafc6085de
Asif <140490107+asifd1253@users.noreply.github.com>
2025-06-30 09:19:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,34 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String input = br.readLine();
-            String[] inputArray = input.split("\\s+");
-            int a[] = new int[inputArray.length];
-
-            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
-
-            Solution ob = new Solution();
-            ob.sort012(a);
-
-            for (int num : a) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-
-// } Driver Code Ends
 class Solution {
     // Function to sort an array of 0s, 1s, and 2s
     public void sort012(int[] arr) {
@@ -36,6 +5,7 @@ class Solution {
         int zero = 0;
         int one = 0;
         int two = 0;
+        
         for(int i=0; i<arr.length; i++){
             if(arr[i] == 0){
                 zero++;
@@ -47,19 +17,17 @@ class Solution {
         }
         
         int idx = 0;
-        for(int i=0; i<zero; i++){
-            arr[idx++] = 0;
+        
+        for(int j=0; j<zero; j++){
+            arr[idx++]  = 0;
         }
         
-        for(int i=0; i<one; i++){
+        for(int k=0; k<one; k++){
             arr[idx++] = 1;
         }
         
-        for(int i=0; i<two; i++){
+        for(int m=0; m<two; m++){
             arr[idx++] = 2;
         }
     }
 }
-
-//{ Driver Code Starts.
-// } Driver Code Ends
