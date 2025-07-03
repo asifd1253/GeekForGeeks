dde77b2cd8d3d3d982eb391cadda5d02c3492722
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-03 14:12:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
@@ -1,50 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.util.*;
-
-public class GFG {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            List<Integer> arr = new ArrayList<>();
-            List<Integer> brr = new ArrayList<>();
-            List<Integer> crr = new ArrayList<>();
-
-            String input1 = sc.nextLine();
-            Scanner ss1 = new Scanner(input1);
-            while (ss1.hasNextInt()) {
-                arr.add(ss1.nextInt());
-            }
-
-            String input2 = sc.nextLine();
-            Scanner ss2 = new Scanner(input2);
-            while (ss2.hasNextInt()) {
-                brr.add(ss2.nextInt());
-            }
-
-            String input3 = sc.nextLine();
-            Scanner ss3 = new Scanner(input3);
-            while (ss3.hasNextInt()) {
-                crr.add(ss3.nextInt());
-            }
-
-            Solution ob = new Solution();
-            List<Integer> res = ob.commonElements(arr, brr, crr);
-
-            if (res.size() == 0) System.out.print(-1);
-            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
-            System.out.println();
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
@@ -80,7 +33,6 @@ class Solution {
         if(res.isEmpty()){
             res.add(-1);
         }
-        
         return res;
     }
 }
