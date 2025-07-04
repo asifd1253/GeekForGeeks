ea6209d8b24d5c13792301496d3cd013361cc596
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-04 07:25:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.java
+++ Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.java
@@ -1,72 +1,34 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-        while (tc-- > 0) {
-            String input = br.readLine();
-            String[] inputArray = input.split("\\s+");
-            ArrayList<Integer> arr = new ArrayList<>();
-
-            for (String s : inputArray) {
-                arr.add(Integer.parseInt(s));
-            }
-
-            new Solution().rearrange(arr);
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
     void rearrange(ArrayList<Integer> arr) {
         // code here
-        ArrayList<Integer> posNum = new ArrayList<>();
-        ArrayList<Integer> negNum = new ArrayList<>();
+        LinkedList<Integer> set1 = new LinkedList<>();
+        LinkedList<Integer> set2 = new LinkedList<>();
         
         for(int i=0; i<arr.size(); i++){
-            if(arr.get(i) >= 0){
-                posNum.add(arr.get(i));
+            int curEle = arr.get(i);
+            
+            if(curEle < 0){
+                set2.add(curEle);
             }else{
-                negNum.add(arr.get(i));
+                set1.add(curEle);
             }
         }
         
-        int i=0, j=0, k=0;
-        while(i < posNum.size() && j < negNum.size()){
-            arr.set(k, posNum.get(i));
-            i++;
-            k++;
-            
-            arr.set(k, negNum.get(j));
-            j++;
-            k++;
-        }
         
-        while(i < posNum.size()){
-            arr.set(k, posNum.get(i));
-            i++;
-            k++;
-        }
-        
-        while(j < negNum.size()){
-            arr.set(k, negNum.get(j));
-            j++;
-            k++;
+        Iterator<Integer> posIter = set1.iterator();
+        Iterator<Integer> negIter = set2.iterator();
+        for(int i=0; i<arr.size(); i++){
+            if(i%2 == 0 && posIter.hasNext()){
+                arr.set(i, posIter.next());
+            }else if(i%2 != 0 && negIter.hasNext()){
+                arr.set(i, negIter.next());
+            }else if(posIter.hasNext()){
+                arr.set(i, posIter.next());
+            }else if(negIter.hasNext()){
+                arr.set(i, negIter.next());
+            }
         }
     }
 }
