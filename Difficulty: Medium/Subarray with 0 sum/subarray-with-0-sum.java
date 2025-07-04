0a47adccbe912e1bd32a9fff521b297172e2c728
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-04 09:05:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.java
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.java
@@ -1,44 +1,17 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t > 0) {
-            String[] inputLine;
-            inputLine = br.readLine().trim().split(" ");
-            int n = inputLine.length;
-            int[] arr = new int[n];
-
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-
-            t--;
-
-            System.out.println(new Solution().findsum(arr) == true ? "true" : "false");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     // Function to check whether there is a subarray present with 0-sum or not.
     static boolean findsum(int arr[]) {
         // Your code here
+        HashSet<Integer> prevSum = new HashSet<>();
         int sum = 0;
-        HashSet<Integer> set = new HashSet<>();
         
         for(int i=0; i<arr.length; i++){
-            sum = sum + arr[i];
-            if(arr[i] == 0 || sum == 0 || set.contains(sum)){
+            sum += arr[i];
+            if(arr[i] == 0 || sum == 0 || prevSum.contains(sum)){
                 return true;
+            }else{
+                prevSum.add(sum);
             }
-            set.add(sum);
         }
         
         return false;
