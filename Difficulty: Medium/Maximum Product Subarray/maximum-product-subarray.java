ba4b9da924ce629e47fc62f612e05f9b83c977ae
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-06 08:59:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
@@ -1,52 +1,30 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
-            String[] inputLine = br.readLine().split(" ");
-            int n = inputLine.length;
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            System.out.println(new Solution().maxProduct(arr));
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to find maximum product subarray
     int maxProduct(int[] arr) {
         // code here
-        int n = arr.length;
-        int leftProduct = 1;
-        int rightProduct = 1;
+        int max = Integer.MIN_VALUE;
+        int product = 1;
         
-        int res = Integer.MIN_VALUE;
-        
-        for(int i=0; i<n; i++){
-            leftProduct *= arr[i];
-            rightProduct *= arr[n-1-i];
+        for(int i=0; i<arr.length; i++){
+            product *= arr[i];
             
-            res = Math.max(res, Math.max(leftProduct, rightProduct));
+            max = Math.max(max, product);
             
-            if(leftProduct == 0){
-                leftProduct = 1;
+            if(product == 0){
+                product = 1;
             }
-            if(rightProduct == 0){
-                rightProduct = 1;
-            }
-            
         }
         
-        return res;
+        product = 1;
+        for(int i=arr.length-1; i>=0; i--){
+            product *= arr[i];
+            
+            max = Math.max(max, product);
+            
+            if(product == 0){
+                product = 1;
+            }
+        }
+        
+        return max;
     }
 }
