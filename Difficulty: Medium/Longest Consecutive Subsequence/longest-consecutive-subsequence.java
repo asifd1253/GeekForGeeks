7c972113cd18d8c67263e1c5d31f83acdf804d44
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-07 07:21:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
@@ -1,63 +1,27 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            // Read first array
-            String input = br.readLine();
-            String[] inputArray = input.split(" ");
-            int[] arr = Arrays.stream(inputArray).mapToInt(Integer::parseInt).toArray();
-
-            Solution ob = new Solution();
-            int res = ob.longestConsecutive(arr);
-
-            System.out.println(res);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
 
     // Function to return length of longest subsequence of consecutive integers.
     public int longestConsecutive(int[] arr) {
         // code here
-        HashMap<Integer, Boolean> map = new HashMap<>();
-        for(int num : arr){
-            map.put(num, false);
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i : arr){
+            set.add(i);
         }
         
-        int longestLength = 0;
+        int maxLen = 0;
         
-        for(int num : arr){
-            int currLength = 1;
-            
-            int nextNum = num+1;
-            while(map.containsKey(nextNum) && map.get(nextNum) == false){
-                currLength++;
-                map.put(nextNum, true);
-                nextNum++;
+        for(int num : set){
+            if(!set.contains(num-1)){
+                 int max = 1;
+                 while(set.contains(num+1)){
+                     max++;
+                     num++;
+                 }
+                 maxLen = Math.max(max, maxLen);
             }
-            
-            int prevNum = num-1;
-            while(map.containsKey(prevNum) && map.get(prevNum) == false){
-                currLength++;
-                map.put(prevNum, true);
-                prevNum--;
-            }
-            
-            longestLength = Math.max(longestLength, currLength);
         }
         
-        
-        return longestLength;
+        return maxLen;
     }
-} 
+}
