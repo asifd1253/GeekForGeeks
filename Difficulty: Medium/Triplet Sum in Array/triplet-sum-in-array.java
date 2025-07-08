d40beecbe594855e1272bb080f62f67db2942f12
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-08 09:15:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.java
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.java
@@ -1,34 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
-
-        while (t-- > 0) {
-            String line = read.readLine().trim(); // Read the array input
-            String[] numsStr = line.split(" ");   // Split the input string by spaces
-            int[] nums =
-                new int[numsStr.length]; // Convert string array to integer array
-            for (int i = 0; i < numsStr.length; i++) {
-                nums[i] = Integer.parseInt(numsStr[i]);
-            }
-
-            int target = Integer.parseInt(read.readLine().trim()); // Read target sum
-
-            Solution ob = new Solution();
-            boolean ans =
-                ob.hasTripletSum(nums, target); // Call the function and store result
-            System.out.println(ans ? "true" : "false"); // Output the result
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
@@ -36,12 +5,11 @@ class Solution {
     // to x in arr[], otherwise false
     public static boolean hasTripletSum(int arr[], int target) {
         // Your code Here
-        int n = arr.length;
         Arrays.sort(arr);
         
-        for(int i=0; i<n; i++){
+        for(int i=0; i<arr.length; i++){
             int left = i+1;
-            int right = n-1;
+            int right = arr.length-1;
             
             while(left < right){
                 if(arr[i] + arr[left] + arr[right] == target){
@@ -52,9 +20,8 @@ class Solution {
                     right--;
                 }
             }
-            
         }
         
-         return false;
+        return false;
     }
 }
