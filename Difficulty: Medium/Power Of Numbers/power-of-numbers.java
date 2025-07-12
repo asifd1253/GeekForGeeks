ef90195c9208d0f49196d7078a54f861e6c08e9a
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-12 10:41:52 +0530

    Update power-of-numbers.java

--- Difficulty: Medium/Power Of Numbers/power-of-numbers.java
+++ Difficulty: Medium/Power Of Numbers/power-of-numbers.java
@@ -1,15 +1,28 @@
 class Solution {
     public int reverseExponentiation(int n) {
         // code here
-        int reverse = 0;
-        int temp = n;
+        // int reverse = 0;
+        // int temp = n;
         
-        while(temp > 0){
-            int lastDigit = temp % 10;
-            reverse = reverse * 10 + lastDigit;
-            temp = temp / 10;
+        // while(temp > 0){
+        //     int lastDigit = temp % 10;
+        //     reverse = reverse * 10 + lastDigit;
+        //     temp = temp / 10;
+        // }
+        
+        // return (int)Math.pow(n, reverse);
+        
+        String s = String.valueOf(n);
+        
+        StringBuilder str = new StringBuilder();
+        
+        for(int i=s.length()-1; i>=0; i--){
+            char ch = s.charAt(i);
+            str.append(ch);
         }
         
-        return (int)Math.pow(n, reverse);
+        int revNum = Integer.valueOf(str.toString());
+        
+        return (int)Math.pow(n, revNum);
     }
 }
