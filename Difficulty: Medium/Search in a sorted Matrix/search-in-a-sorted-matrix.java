c9d39a05c8310a59743b22d288d8d870d00b7137
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-14 07:22:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.java
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.java
@@ -1,59 +1,19 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t > 0) {
-            int rows = sc.nextInt();
-            int columns = sc.nextInt();
-
-            int matrix[][] = new int[rows][columns];
-
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < columns; j++) {
-                    matrix[i][j] = sc.nextInt();
-                }
-            }
-            int target = sc.nextInt();
-
-            Solution x = new Solution();
-
-            if (x.searchMatrix(matrix, target))
-                System.out.println("true");
-            else
-                System.out.println("false");
-            t--;
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 
 
 class Solution {
     // Function to search a given number in row-column sorted matrix.
-    public boolean searchMatrix(int[][] mat, int key) {
+    public boolean searchMatrix(int[][] mat, int x) {
         // code here
-        int row =0,col = mat[0].length-1;
+        int row = 0;
+        int col = mat[0].length-1;
         
-        while(row < mat.length && col >= 0){
-            if(mat[row][col] == key){
+        while(row <= mat.length-1 && col >= 0){
+            if(mat[row][col] == x){
                 return true;
-            }
-            
-            else if(key < mat[row][col]){
-                col--;
-            }
-            
-            else{
+            }else if(x > mat[row][col]){
                 row++;
+            }else{
+                col--;
             }
         }
         
