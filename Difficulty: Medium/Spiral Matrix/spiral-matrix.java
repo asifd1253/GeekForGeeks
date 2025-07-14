611fab23d916dddb065c37af2e3e360ec8ee3262
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-14 07:02:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spiral Matrix/spiral-matrix.java
+++ Difficulty: Medium/Spiral Matrix/spiral-matrix.java
@@ -1,88 +1,50 @@
-//{ Driver Code Starts
-import java.util.*;
+// User function Template for Java
 
-class Find_Given_Element_Of_Spiral_Matrix 
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while(t > 0)
-		{
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-			int k = sc.nextInt();
-			int arr[][] = new int[n][m];
-			for(int i=0; i<n; i++)
-			{
-				for(int j=0; j<m; j++ )
-				{
-					arr[i][j] = sc.nextInt();
-				}
-			}
-			Solution obj = new Solution();
-			System.out.println(obj.findK(arr, n, m, k));
-		t--;
-		
-System.out.println("~");
-}
-	}
-}
-// } Driver Code Ends
+class Solution {
 
-
-//User function Template for Java
-
-class Solution
-{
-   
-	public int findK(int a[][], int n, int m, int k)
-	{
-	    // Your code goes here
-	    int arr[] = new int[n*m];
-	    int arrCount =0;
-	    
-	    int startRow = 0;
-	    int startCol = 0;
-	    int endRow = n-1;
-	    int endCol = m-1;
-	    
-	    while(startRow <= endRow && startCol <= endCol){
-	        //top
-	        for(int j=startCol; j<=endCol; j++){
-	            arr[arrCount] = a[startRow][j];
-	            arrCount++;
-	        }
-	        startRow++;
-	        
-	        //right
-	        for(int i=startRow; i<=endRow; i++){
-	            arr[arrCount] = a[i][endCol];
-	            arrCount++;
-	        }
-	        endCol--;
-	        
-	        //bottom
-	        if(startRow <= endRow){
-	            for(int j=endCol; j>=startCol; j--){
-    	            arr[arrCount] = a[endRow][j];
-    	            arrCount++;
-    	        }
-    	        endRow--;
-	        }
-	        
-	        //left
-	        if(startCol <= endCol){
-	            for(int i=endRow; i>=startRow; i--){
-	            arr[arrCount] = a[i][startCol];
-	            arrCount++;
-	        }
-	        startCol++;
-	        }
-	        
-	    }
-	    
-	    return arr[k-1];
-	}
-	
+    public int findK(int arr[][], int n, int m, int k) {
+        // Your code goes here
+        
+        int res[] = new int[n*m];
+        int idx = 0;
+        
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = arr.length-1;
+        int endCol = arr[0].length-1;
+        
+        while(startRow <= endRow && startCol <= endCol){
+            //top 
+            for(int i=startCol; i<= endCol; i++){
+                res[idx++] = arr[startRow][i];
+            }
+            startRow++;
+            
+            //right
+            for(int i=startRow; i<=endRow; i++){
+                res[idx++] = arr[i][endCol];
+            }
+            endCol--;
+            
+            // bottom
+            if(startRow <= endRow){
+                for(int i=endCol; i>=startCol; i--){
+                    res[idx++] = arr[endRow][i];
+                }
+                endRow--;
+            }
+            
+            
+            //left
+            if(startCol <= endCol){
+                for(int i=endRow; i>=startRow; i--){
+                    res[idx++] = arr[i][startCol];
+                }
+                startCol++;    
+            }
+            
+        }
+        
+        return res[k-1];
+    }
 }
