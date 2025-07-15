19e4d374adb502e3643c7af59d45718ae83242b5
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-15 20:11:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.java
+++ Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.java
@@ -1,49 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(in.readLine().trim());
-            int a[][] = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                String s[] = in.readLine().trim().split(" ");
-                for (int j = 0; j < n; j++) {
-                    a[i][j] = Integer.parseInt(s[j]);
-                }
-            }
-            Solution ob = new Solution();
-            ob.transpose(n, a);
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    out.print(a[i][j] + " ");
-                }
-                out.println();
-            }
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    public void transpose(int n, int mat[][]) {
-        for(int i=0; i<mat.length; i++){
-            for(int j=i+1; j<mat[0].length; j++){
-                int temp = mat[i][j];
-                mat[i][j] = mat[j][i];
-                mat[j][i] = temp;
+    public ArrayList<ArrayList<Integer>> transpose(int[][] mat) {
+        // code here
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        
+        for(int c=0; c<mat[0].length; c++){
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int r=0; r<mat.length; r++){
+                temp.add(mat[r][c]);
             }
+            res.add(new ArrayList<>(temp));
         }
+        
+        return res;
     }
 }
