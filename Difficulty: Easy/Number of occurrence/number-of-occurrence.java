54c89368dc0a3e0b546d07f98aaeeb39afe11cbd
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-19 09:13:16 +0530

    Update number-of-occurrence.java

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -14,3 +14,41 @@ class Solution {
         return map.get(target);
     }
 }
+
+//  Using 2nd Solution by Binary Search 
+
+// class Solution {
+//     int binarySearch(int arr[], int target, boolean searchFirst){
+//         int start = 0;
+//         int end = arr.length-1;
+//         int ans = -1;
+        
+//         while(start <= end){
+//             int mid = start + (end - start)/2;
+            
+//             if(arr[mid] == target){
+//                 ans = mid;
+//                 if(searchFirst){
+//                     end = mid-1;
+//                 }else{
+//                     start = mid+1;
+//                 }
+//             }else if(arr[mid] > target){
+//                 end = mid-1;
+//             }else{
+//                 start = mid+1;
+//             }
+//         }
+        
+//         return ans;
+//     }
+//     int countFreq(int[] arr, int target) {
+//         int first = binarySearch(arr, target, true);
+//         int last = binarySearch(arr, target, false);
+        
+//         if(first == -1 || last == -1){
+//             return 0;
+//         }
+//         return last - first +1;
+//     }
+// }
