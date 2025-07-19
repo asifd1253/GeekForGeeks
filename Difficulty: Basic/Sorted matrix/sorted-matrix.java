9c869311c5972934bef954ec5a282368f648ad51
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-19 10:52:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted matrix/sorted-matrix.java
+++ Difficulty: Basic/Sorted matrix/sorted-matrix.java
@@ -1,61 +1,24 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            int N = Integer.parseInt(read.readLine());
-            int v[][] = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                String s[] = read.readLine().split(" ");
-                for (int j = 0; j < N; j++) v[i][j] = Integer.parseInt(s[j]);
-            }
-            Solution ob = new Solution();
-            int ans[][] = ob.sortedMatrix(N, v);
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) System.out.print(ans[i][j] + " ");
-                System.out.println();
-            }
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
     int[][] sortedMatrix(int N, int Mat[][]) {
         // code here
-        int res[][] = new int[N][N];
-        int rows = Mat.length;
-        int cols = Mat[0].length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        ArrayList<Integer> arr = new ArrayList<>();
-        
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                arr.add(Mat[i][j]);
+        for(int i=0; i<Mat.length; i++){
+            for(int j=0; j<Mat[0].length; j++){
+                pq.add(Mat[i][j]);
             }
         }
         
-        Collections.sort(arr);
-        int k = 0;
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                res[i][j] = arr.get(k);
-                k++;
+        //Restore the elements
+        for(int i=0; i<Mat.length; i++){
+            for(int j=0; j<Mat[0].length; j++){
+                Mat[i][j] = pq.peek();
+                pq.remove();
             }
         }
         
-        return res;
+        return Mat;
     }
 };
