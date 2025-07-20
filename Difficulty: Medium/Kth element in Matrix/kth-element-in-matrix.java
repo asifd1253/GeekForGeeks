e7fce9644227b3dec8c9704cb9e92d614cf007ba
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-20 11:23:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth element in Matrix/kth-element-in-matrix.java
+++ Difficulty: Medium/Kth element in Matrix/kth-element-in-matrix.java
@@ -1,48 +1,18 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
-class KthSmallestElement{
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int t=Integer.parseInt(sc.next());
-		while(t>0)
-		{
-			int n=Integer.parseInt(sc.next());
-			int[][] a=new int[n][n];
-			for(int i=0;i<n;i++)
-				for(int j=0;j<n;j++)
-    				a[i][j]=Integer.parseInt(sc.next());
-    		
-			int k=Integer.parseInt(sc.next());
-		    Solution ob = new Solution();
-		    System.out.println(ob.kthSmallest(a,n,k));
-		    
-		    t--;
-		
-System.out.println("~");
-}
-	}
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    public static int kthSmallest(int[][] mat, int n, int k) {
-        // code here.
-        ArrayList<Integer> arr = new ArrayList<>();
+    public int kthSmallest(int[][] mat, int k) {
+        // code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                arr.add(mat[i][j]);
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
+                pq.add(mat[i][j]);
             }
         }
         
-        Collections.sort(arr);
-    
-        return arr.get(k-1);
+        for(int i=1; i<k; i++){
+            pq.remove();
+        }
+        
+        return pq.peek();
     }
 }
