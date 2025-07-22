8f94d2aafa3e224546fcabb08390a3bf40a9eb90
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-22 20:04:57 +0530

    Update solution.java

--- Difficulty: Easy/Compress the String/solution.java
+++ Difficulty: Easy/Compress the String/solution.java
@@ -1,25 +1,27 @@
-import java.util.*;
-import java.io.*;
+import java.util.* ;
+import java.io.*; 
 
 public class Solution {
-    public static String compressTheString(String s) {
-        // Write your code here
-        StringBuilder newStr = new StringBuilder();
+	public static String compressTheString(String s) {
+		// Write your code here
+		StringBuilder str = new StringBuilder();
 
-        for (int i = 0; i < s.length(); i++) {
-            Integer count = 1;
-            while (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
-                count++;
-                i++;
-            }
-            newStr.append(s.charAt(i));
+		for(int i=0; i<s.length(); i++){
+			char ch = s.charAt(i);
+			Integer count = 1;
+			while(i+1 < s.length() && s.charAt(i+1) == ch){
+				count++;
+				i++;
+			}
 
-            if (count > 1) {
-                newStr.append(count.toString());
-            }
-        }
+			if(count == 1){
+				str.append(ch);
+			}else{
+				str.append(ch).append(count.toString());
+			}
+		}
 
-        return newStr.toString();
-    }
+		return str.toString();
+	}
 
 }
