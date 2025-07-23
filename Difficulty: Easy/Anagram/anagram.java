ba3309afb0ab917cb05dd34d90052bc12063b696
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-23 17:38:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -1,49 +1,29 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-import java.util.stream.*;
-
-class GFG {
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String s1 = br.readLine(); // first string
-            String s2 = br.readLine(); // second string
-
-            Solution obj = new Solution();
-
-            if (obj.areAnagrams(s1, s2)) {
-                System.out.println("true");
-            } else {
-                System.out.println("false");
-            }
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    // Function is to check whether two strings are anagram of each other or not.
     public static boolean areAnagrams(String s1, String s2) {
-
-        // Your code here
-        if(s1.length() != s2.length()){
+        // code here
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<s1.length(); i++){
+            char ch = s1.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+        
+        for(int i=0; i<s2.length(); i++){
+            char ch = s2.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch, map.getOrDefault(ch, 0)-1);
+                if(map.get(ch) == 0){
+                    map.remove(ch);
+                }
+            }else{
+                return false;
+            }
+        }
+        
+        if(map.size() != 0){
             return false;
         }
         
-        char arr1[] = s1.toCharArray();
-        char arr2[] = s2.toCharArray();
-        
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        
-        return Arrays.equals(arr1,arr2);
-        
+        return true;
     }
 }
