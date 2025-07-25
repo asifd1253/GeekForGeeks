a1e89a2a8857682d33dc43c119fc3fe7eeeb616a
Asif <140490107+asifd1253@users.noreply.github.com>
2025-07-25 07:36:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
@@ -1,69 +1,28 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-
-        int t = Integer.parseInt(in.readLine().trim());
-        while (t-- > 0) {
-            String line = in.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            int key = Integer.parseInt(in.readLine().trim());
-
-            out.println(new Solution().search(arr, key));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    int search(int[] arr, int key) {
-        // Complete this function
-        int st = 0;
+    int search(int[] arr, int target) {
+        // code here
+        int start = 0;
         int end = arr.length-1;
         
-        while(st <= end){
-            int mid = st + (end - st)/2;
+        while(start <= end){
+            int mid = start + (end - start)/2;
             
-            if(key == arr[mid]){
+            if(arr[mid] == target){
                 return mid;
             }
             
-            if(arr[st] <= arr[mid]){
-                if(key <= arr[mid] && arr[st] <= key){
-                    end = mid-1;
+            //first sorted array
+            if(arr[start] <= arr[mid]){
+                if(arr[start] <= target && target < arr[mid]){
+                    end = mid - 1;
                 }else{
-                    st = mid+1;
+                    start = mid + 1;
                 }
             }else{
-                if(key >= arr[mid] && arr[end] >= key){
-                    st = mid+1;
+                if(arr[mid] < target && target <= arr[end]){
+                    start = mid + 1;
                 }else{
-                    end = mid-1;
+                    end = mid - 1;
                 }
             }
         }
