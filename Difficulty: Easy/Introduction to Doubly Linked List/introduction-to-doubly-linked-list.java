134385d218b0506a1a2d7f931f81d6d79569852d
Asif <140490107+asifd1253@users.noreply.github.com>
2025-08-02 10:37:51 +0530

    Update introduction-to-doubly-linked-list.java

--- Difficulty: Easy/Introduction to Doubly Linked List/introduction-to-doubly-linked-list.java
+++ Difficulty: Easy/Introduction to Doubly Linked List/introduction-to-doubly-linked-list.java
@@ -2,18 +2,19 @@
 
 class Solution {
     Node constructDLL(int arr[]) {
-        // Code here
-        Node head = new Node(arr[arr.length-1]);
-        
-        for(int i=arr.length-2; i>=0; i--){
-            Node temp = new Node(arr[i]);
-            
-            temp.next = head;
-            head.prev = temp;
-            temp.prev = null;
-            head = temp;
-        }
-        
-        return head;
+       Node head = new Node(arr[0]);
+       
+       Node curr = head;
+       
+       for(int i=1; i<arr.length; i++){
+           Node temp = new Node(arr[i]);
+           
+           curr.next = temp;
+           temp.prev = curr;
+           temp.next = null;
+           curr = temp;
+       }
+       
+       return head;
     }
 }
