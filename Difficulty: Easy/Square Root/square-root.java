6edad10a46043bd45592df9aad820c4c513a6758
Asif <140490107+asifd1253@users.noreply.github.com>
2025-08-12 06:32:55 +0530

    Update square-root.java

--- Difficulty: Easy/Square Root/square-root.java
+++ Difficulty: Easy/Square Root/square-root.java
@@ -1,10 +1,23 @@
 class Solution {
     int floorSqrt(int n) {
-        int res = 0;
-        for(int i=1; i*i<=n; i++){
-            res = i;
+        if(n == 0 || n == 1){
+            return n;
         }
         
+        int res = 0;
+        
+        int start = 1, end = n;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            
+            if(mid <= n/mid){
+                res = mid;
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
         return res;
     }
 }
