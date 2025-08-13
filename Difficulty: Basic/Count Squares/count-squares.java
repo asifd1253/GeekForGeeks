a2eeb1889de46a65392c94776f50e4c3e69b7e3d
Asif <140490107+asifd1253@users.noreply.github.com>
2025-08-13 06:52:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Squares/count-squares.java
+++ Difficulty: Basic/Count Squares/count-squares.java
@@ -2,13 +2,26 @@
 
 class Solution {
     static int countSquares(int n) {
-        // code here
-        int count = 0;
         
-        for(int i=1; i*i<n; i++){
-            count++;   
+        // int count = 0;
+        // for(int i=1; i*i<n; i++){
+        //     count++;
+        // }
+        // return count;
+        int start = 1;
+        int end = n; 
+        int ans = 0;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if (mid <= (n - 1) / mid) { // equivalent to mid*mid < n
+                ans = mid;
+                start = mid + 1;  // move right to find larger candidate
+            }else{
+                end = mid - 1;
+            }
         }
-        
-        return count;
+        return ans;
     }
 }
