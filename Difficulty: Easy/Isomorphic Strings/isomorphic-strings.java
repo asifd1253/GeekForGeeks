73740a7b3a74e90be93443bd86a6fbceb24d0997
Asif <140490107+asifd1253@users.noreply.github.com>
2025-08-23 19:34:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Isomorphic Strings/isomorphic-strings.java
+++ Difficulty: Easy/Isomorphic Strings/isomorphic-strings.java
@@ -1,29 +1,27 @@
-import java.util.*;
 class Solution {
-    public static boolean areIsomorphic(String s1, String s2) {
+    public boolean areIsomorphic(String s1, String s2) {
         // code here
         if(s1.length() != s2.length()){
             return false;
         }
         
-        HashMap<Character, Character> map1 = new HashMap<>();
-        HashMap<Character, Character> map2 = new HashMap<>();
+        int map1[] = new int[26];
+        int map2[] = new int[26];
+        
+        Arrays.fill(map1, -1);
+        Arrays.fill(map2, -1);
         
         for(int i=0; i<s1.length(); i++){
-            char s1char = s1.charAt(i);
-            char s2char = s2.charAt(i);
+            int c1 = s1.charAt(i) - 'a';
+            int c2 = s2.charAt(i) - 'a';
             
-            if(map1.containsKey(s1char)){
-                if(map1.get(s1char) != s2char){
-                    return false;
-                }
+            if(map1[c1] == -1 && map2[c2] == -1){
+                map1[c1] = c2;
+                map2[c2] = c1;
             }else{
-                if(map2.containsKey(s2char)){
+                if(map1[c1] != c2 || map2[c2] != c1){
                     return false;
                 }
-                
-                map1.put(s1char, s2char);
-                map2.put(s2char, s1char);
             }
         }
         
