2cbd09206a2972c2cb5dfbb5f1665312bc82ab62
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-09-16 10:37:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Convert a sentence into its equivalent mobile numeric keypad sequence/convert-a-sentence-into-its-equivalent-mobile-numeric-keypad-sequence.java
+++ Difficulty: Easy/Convert a sentence into its equivalent mobile numeric keypad sequence/convert-a-sentence-into-its-equivalent-mobile-numeric-keypad-sequence.java
@@ -1,42 +1,28 @@
-//{ Driver Code Starts
-//Initial Template for Java
-import java.io.*;
-import java.util.*; 
-class GFG{
-    public static void main(String args[]) throws IOException { 
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while(t-- > 0){
-            String S = read.readLine();
-            Solution obj = new Solution();
-            System.out.println(obj.printSequence(S));
-            
-        }
-    } 
-} 
-// } Driver Code Ends
-
-
 // User function Template for Java
 class Solution {
     String printSequence(String s) {
         // code here
-        String arr[] = {"2",   "22",  "222", "3",   "33",   "333", "4",
-            "44",  "444", "5",   "55",  "555",  "6",   "66",
-            "666", "7",   "77",  "777", "7777", "8",   "88",
-            "888", "9",   "99",  "999", "9999"};
-            
-        StringBuilder str = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        
+        String refArray[] = {
+            "2", "22", "222", "3", "33", "333",
+            "4", "44", "444", "5", "55", "555", "6", "66", "666",
+            "7", "77", "777", "7777", "8", "88", "888", "9", "99", "999", "9999"
+        };
         
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == ' '){
-                str.append('0');
-            }else{
-                int idx = s.charAt(i) - 'A';
-                str.append(arr[idx]);
+            int curChar = s.charAt(i);
+            
+            if(curChar == ' '){
+                sb.append("0");
+                continue;
             }
+            
+            int idx = curChar - 'A';
+            
+            sb.append(refArray[idx]);
         }
         
-        return str.toString();
+        return sb.toString();
     }
 }
