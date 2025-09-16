77c27e0918af75bb9eb24865bdc4a745116c5e11
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-09-16 10:22:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,60 +1,28 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        // Reading total number of testcases
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            // reading the string
-            String st = sc.next();
-
-            // calling ispar method of Paranthesis class
-            // and printing "balanced" if it returns true
-            // else printing "not balanced"
-            if (new Solution().isBalanced(st) == true)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
-    static boolean isBalanced(String s) {
+    public boolean isBalanced(String s) {
         // code here
         Stack<Character> st = new Stack<>();
         
         for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
+            char curChar = s.charAt(i);
             
-            if(ch == '(' || ch == '{' || ch == '['){
-                st.push(ch);
-                continue;
+            if(curChar == '(' || curChar == '[' || curChar == '{'){
+                st.push(curChar);
             }else if(st.isEmpty()){
                 return false;
-            }else if(ch == ')' && st.peek() == '('){
-                st.pop();
-            }else if(ch == '}' && st.peek() == '{'){
-                st.pop();
-            }else if(ch == ']' && st.peek() == '['){
-                st.pop();
-            }else{
-                return false;
+            }else {
+                if(curChar == ')' && st.peek() == '('){
+                    st.pop();
+                }else if(curChar == '}' && st.peek() == '{'){
+                    st.pop();
+                }else if(curChar == ']' && st.peek() == '['){
+                    st.pop();
+                }else{
+                    return false;
+                }
             }
         }
         
-        
-        return st.isEmpty();
+        return st.isEmpty()?true:false;
     }
 }
