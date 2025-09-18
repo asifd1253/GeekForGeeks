1d8a5fe49fadc46d5ecdc260c1b5e8340a01d402
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-09-18 10:56:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.java
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.java
@@ -1,30 +1,4 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int tc = Integer.parseInt(br.readLine().trim());
-        while (tc-- > 0) {
-            String s = br.readLine().trim();
-            Solution ob = new Solution();
-            System.out.println(ob.romanToDecimal(s));
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Finds decimal value of a given roman numeral
     public int romanToDecimal(String s) {
         // code here
         HashMap<Character, Integer> map = new HashMap<>();
@@ -40,18 +14,19 @@ class Solution {
         int result = 0;
         
         for(int i=0; i<s.length(); i++){
-            int curr = map.get(s.charAt(i));
             
-            if(i+1 < s.length()){
-                int next = map.get(s.charAt(i+1));
-                
-                if(curr < next){
-                    result -= curr;
-                }else{
-                    result += curr;
-                }
+            if(i == s.length()-1){
+                result += map.get(s.charAt(i));
+                break;
+            }
+            
+            int curVal = map.get(s.charAt(i));
+            int nextVal = map.get(s.charAt(i+1));
+            
+            if(curVal >= nextVal){
+                result = result + curVal;
             }else{
-                result += curr;
+                result = result - curVal;
             }
         }
         
