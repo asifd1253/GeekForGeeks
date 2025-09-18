46bed3d3400c1d6e4a2f0f37279a3e390223be13
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-09-18 11:42:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second most repeated string in a sequence/second-most-repeated-string-in-a-sequence.java
+++ Difficulty: Easy/Second most repeated string in a sequence/second-most-repeated-string-in-a-sequence.java
@@ -1,31 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- > 0)
-        {
-            int n = sc.nextInt();
-            String arr[] = new String[n];
-            for(int i = 0;i<n;i++)
-                arr[i] = sc.next();
-            
-            Solution ob = new Solution();
-            
-            System.out.println(ob.secFrequent(arr, n));
-        }
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
@@ -33,32 +5,31 @@ class Solution {
         // your code here
         HashMap<String, Integer> map = new HashMap<>();
         
-        for(int i=0; i<N; i++){
-            map.put(arr[i],map.getOrDefault(arr[i], 0) + 1);
+        for(String str : arr){
+            map.put(str, map.getOrDefault(str, 0)+1);
         }
         
-        int freq1 = 0;
-        int freq2 = 0;
+        int firFreq = 0;
+        int secFreq = 0;
         
         String str1 = "";
         String str2 = "";
         
-        for(String word : map.keySet()){
-            int currfreq = map.get(word);
+        for(String str : arr){
+            int curFreq = map.get(str);
             
-            if(freq1 < currfreq){
-                freq2 = freq1;
+            if(firFreq < curFreq){
+                secFreq = firFreq;
                 str2 = str1;
                 
-                freq1 = currfreq;
-                str1 = word;
-            }else if(freq1 > currfreq && freq2 < currfreq){
-                freq2 = currfreq;
-                str2 = word;
+                firFreq = curFreq;
+                str1 = str;
+            }else if(firFreq > curFreq && secFreq < curFreq){
+                secFreq = curFreq;
+                str2 = str;
             }
         }
-        
+       
         return str2;
-        
     }
 }
