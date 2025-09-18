3a1e9fef93cccaeac4c736e8ece33dbdc0734a33
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-09-18 10:24:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Count occurrences of a given word in a 2-d array/count-occurrences-of-a-given-word-in-a-2d-array.java
+++ Difficulty: Hard/Count occurrences of a given word in a 2-d array/count-occurrences-of-a-given-word-in-a-2d-array.java
@@ -1,93 +1,11 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG
-{
-    static class FastReader{ 
-        BufferedReader br; 
-        StringTokenizer st; 
-  
-        public FastReader(){ 
-            br = new BufferedReader(new InputStreamReader(System.in)); 
-        } 
-  
-        String next(){ 
-            while (st == null || !st.hasMoreElements()){ 
-                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
-            } 
-            return st.nextToken(); 
-        } 
-  
-        String nextLine(){ 
-            String str = ""; 
-            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
-            return str; 
-        } 
-        
-        Integer nextInt(){
-            return Integer.parseInt(next());
-        }
-    } 
-    
-    public static void main(String args[])
-    {
-        FastReader sc = new FastReader();
-        int t = sc.nextInt();
-        while(t>0)
-        {
-            int R = sc.nextInt();
-            int C = sc.nextInt();
-            
-            char mat[][] = new char[R][C];
-            for(int i=0;i<R;i++)
-            {
-                for(int j=0;j<C;j++)
-                {
-                    mat[i][j] = sc.next().charAt(0);
-                }
-            }
-            String target  = sc.next();
-            Solution obj = new Solution();
-            System.out.println(obj.findOccurrence(mat,target));
-            
-            t--;
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
-    int R;
-    int C;
-    public int findOccurrence(char mat[][], String target) {
-        // Write your code here
-        R = mat.length;
-        C = mat[0].length;
-        int count = 0;
-        boolean visited[][] = new boolean[R][C];
-        
-        for(int i=0; i<R; i++){
-            for(int j=0; j<C; j++){
-                if(mat[i][j] == target.charAt(0)){
-                    count += dfs(mat, visited, 0, i, j, target);
-                }
-            }
-        }
-        
-        return count;
-    }
+    int rows;
+    int cols;
     
-    public int dfs(char mat[][], boolean visited[][], int idx, int i, int j, String target){
-        if(i<0 || i>=R || j<0 || j>=C || visited[i][j] == true || mat[i][j] != target.charAt(idx)){
+    int dfs(char mat[][], String target, boolean visited[][], int row, int col, int idx){
+        if(row<0 || row>=rows || col<0 || col>=cols || visited[row][col] == true || mat[row][col] != target.charAt(idx)){
             return 0;
         }
         
@@ -97,14 +15,34 @@ class Solution {
         
         int found = 0;
         
-        visited[i][j] = true;
-        found += dfs(mat, visited,idx+1, i-1, j, target);
-        found += dfs(mat, visited,idx+1, i, j+1, target);
-        found += dfs(mat, visited,idx+1, i+1, j, target);
-        found += dfs(mat, visited,idx+1, i, j-1, target);
+        visited[row][col] = true;
         
-        visited[i][j] = false;
+        found += dfs(mat, target, visited, row-1, col, idx+1);
+        found += dfs(mat, target, visited, row, col+1, idx+1);
+        found += dfs(mat, target, visited, row+1, col, idx+1);
+        found += dfs(mat, target, visited, row, col-1, idx+1);
+        
+        visited[row][col] = false;
         
         return found;
+    }
+    
+    public int findOccurrence(char mat[][], String target) {
+        // Write your code here
+        rows = mat.length;
+        cols = mat[0].length;
+        
+        int count = 0;
+        boolean visited[][] = new boolean[rows][cols];
+        
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(target.charAt(0) == mat[i][j]){
+                    count += dfs(mat, target, visited, i, j, 0);
+                }
+            }
+        }
+        
+        return count;
     }
 }
