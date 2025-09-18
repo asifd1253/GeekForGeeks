3e77514d5c684997d603c91344a2743e6c911464
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-09-18 11:16:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Min Number of Flips/min-number-of-flips.java
+++ Difficulty: Easy/Min Number of Flips/min-number-of-flips.java
@@ -1,48 +1,22 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class GFG
-{
-	public static void main(String[] args) throws IOException
-	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while(t-->0)
-        {
-            String S = br.readLine().trim();
-            Solution ob = new Solution();
-            
-            System.out.println(ob.minFlips(S));
-                        
-        }
-	}
-}
-
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    public int minFlips(String S) {
+    public int minFlips(String s) {
         // Code here
-        int flip1 = 0;
-        int flip2 = 0;
+        int patOneFlip = 0;
+        int patTwoFlip = 0;
         
-        for(int i=0; i<S.length(); i++){
-            char expected1 = (i%2 == 0) ? '0' : '1';
-            char expected2 = (i%2 == 0) ? '1' : '0';
-            
-            if(expected1 != S.charAt(i)){
-                flip1++;
+        for(int i=0; i<s.length(); i++){
+            //pattern 1 ==> 010101010
+            if(i%2 == 0){
+                if(s.charAt(i) != '0') patOneFlip++;
+                if(s.charAt(i) != '1') patTwoFlip++;
             }
-            if(expected2 != S.charAt(i)){
-                flip2++;
+            //pattern 2 ==> 101010101
+            else{
+                if(s.charAt(i) != '1') patOneFlip++;
+                if(s.charAt(i) != '0') patTwoFlip++;
             }
         }
         
-        return Math.min(flip1, flip2);
+        return Math.min(patOneFlip, patTwoFlip);
     }
 }
