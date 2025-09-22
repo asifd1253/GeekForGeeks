81f048521cba0f99d0f5c0bcc645d04f2dd2f4d0
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-09-22 11:43:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Substrings with same first and last characters/substrings-with-same-first-and-last-characters.java
+++ Difficulty: Easy/Substrings with same first and last characters/substrings-with-same-first-and-last-characters.java
@@ -1,18 +1,17 @@
 class Solution {
     public int countSubstring(String s) {
         // code here
-       
         int freq[] = new int[26];
         
-        for(int i=0; i<s.length(); i++){
-            freq[s.charAt(i) - 'a']++;
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
         }
         
-        int count = 0;
-        for(int j=0; j<26; j++){
-            count += (freq[j]*(freq[j]+1))/2;
+        int res = 0;
+        for(int f : freq){
+            res += (f * (f+1))/2;
         }
         
-        return count;
+        return res;
     }
 }
