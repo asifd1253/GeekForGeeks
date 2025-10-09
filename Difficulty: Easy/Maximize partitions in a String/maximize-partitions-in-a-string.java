8d82c5d78c3cbc62987a04240a0e31c85e8e2cb9
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-10-09 10:16:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Maximize partitions in a String/maximize-partitions-in-a-string.java
+++ Difficulty: Easy/Maximize partitions in a String/maximize-partitions-in-a-string.java
@@ -1,24 +1,25 @@
 class Solution {
     public int maxPartitions(String s) {
         // code here
-        int arr[] = new int[26];
+        int lastIdx[] = new int[26];
         
         for(int i=0; i<s.length(); i++){
-            arr[s.charAt(i) - 'a'] = i;
+            lastIdx[s.charAt(i) - 'a'] = i;
         }
         
-        int totalParti = 0;
-        int maxLast = 0;
+        int maxLastIdx = 0;
+        int totalPart = 0;
         
         for(int i=0; i<s.length(); i++){
-            int idx = s.charAt(i)-'a';
-            maxLast = Math.max(maxLast, arr[idx]);
+            int idx = s.charAt(i) - 'a';
             
-            if(maxLast == i){
-                totalParti++;
+            maxLastIdx = Math.max(maxLastIdx, lastIdx[idx]);
+            
+            if(maxLastIdx == i){
+                totalPart++;
             }
         }
         
-        return totalParti;
+        return totalPart;
     }
 }
