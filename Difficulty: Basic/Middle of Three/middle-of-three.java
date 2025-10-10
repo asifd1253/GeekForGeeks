cc737cc84652d3aa2704a881e20f244f870f6e88
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-10-10 10:06:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Middle of Three/middle-of-three.java
+++ Difficulty: Basic/Middle of Three/middle-of-three.java
@@ -3,19 +3,13 @@
 class Solution {
     int middle(int a, int b, int c) {
         // code here
-        // if((a<b && a>c) || (a>b && a<c)){
-        //     return a;
-        // }else if((b<c && b>a) || (b>c && b<a)){
-        //     return b;
-        // }else{
-        //     return c;
-        // }
-        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
         pq.add(a);
         pq.add(b);
         pq.add(c);
         pq.remove();
+        
         return pq.peek();
     }
 }
