6da979f5ddbb2b89fa43a67298f731617ad221e1
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-10-10 10:06:56 +0530

    Use PriorityQueue to find the middle of three numbers
    Refactor middle method to find the middle value using PriorityQueue.

--- Difficulty: Basic/Middle of Three/middle-of-three.java
+++ Difficulty: Basic/Middle of Three/middle-of-three.java
@@ -3,13 +3,19 @@
 class Solution {
     int middle(int a, int b, int c) {
         // code here
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // if((a<b && a>c) || (a>b && a<c)){
+        //     return a;
+        // }else if((b<c && b>a) || (b>c && b<a)){
+        //     return b;
+        // }else{
+        //     return c;
+        // }
         
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.add(a);
         pq.add(b);
         pq.add(c);
         pq.remove();
-        
         return pq.peek();
     }
 }
