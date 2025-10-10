7b5ab63789ed6d7af02437ec21ba47a7b46e1585
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-10-10 09:38:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -1,9 +1,9 @@
 // User function Template for Java
 
 class GFG {
-    public int findOccurence(int arr[], int x, boolean searchFirst){
+    int search(int arr[], int x, boolean whichSide){
         int st = 0;
-        int end = arr.length-1;
+        int end = arr.length - 1;
         int ans = -1;
         
         while(st <= end){
@@ -11,8 +11,7 @@ class GFG {
             
             if(arr[mid] == x){
                 ans = mid;
-                
-                if(searchFirst){
+                if(whichSide == true){
                     end = mid-1;
                 }else{
                     st = mid+1;
@@ -26,13 +25,12 @@ class GFG {
         
         return ans;
     }
-    
     ArrayList<Integer> find(int arr[], int x) {
         // code here
         ArrayList<Integer> res = new ArrayList<>();
         
-        int firstOccur = findOccurence(arr, x, true);
-        int lastOccur = findOccurence(arr, x, false);
+        int firstOccur = search(arr, x, true);
+        int lastOccur = search(arr, x, false);
         
         res.add(firstOccur);
         res.add(lastOccur);
