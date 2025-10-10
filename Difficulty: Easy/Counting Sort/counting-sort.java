2c3baf846026e4552d84ba7f6c126d2788401305
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-10-10 09:22:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Counting Sort/counting-sort.java
+++ Difficulty: Easy/Counting Sort/counting-sort.java
@@ -1,52 +1,23 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
-
-class CountSort
-{
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        
-        while(t-- > 0)
-        {
-            long n = sc.nextLong();
-            String arr = "";
-            arr = sc.next();
-            Solution obj = new Solution();
-            System.out.println(obj.countSort(arr));
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
-
-
-class Solution
-{
-    //Function to arrange all letters of a string in lexicographical 
-    //order using Counting Sort.
-    public static String countSort(String s)
-    {
+class Solution {
+    public static String countSort(String s) {
         // code here
-        StringBuilder str = new StringBuilder();
-        
-        int auxArr[] = new int[26];   //By default the frequency is zero
+        int freq[] = new int[26];
         
         for(int i=0; i<s.length(); i++){
-            char curr = s.charAt(i);
-            auxArr[curr - 'a']++;
+            int idx = s.charAt(i) - 'a';
+            freq[idx]++;
         }
         
+        StringBuilder sb = new StringBuilder();
+        
         for(int i=0; i<26; i++){
-            while(auxArr[i] > 0){
-                str.append((char)(i+'a'));
-                auxArr[i]--;
+            int curCharCount = freq[i];
+            while(curCharCount > 0){
+                sb.append((char)('a'+i));
+                curCharCount--;
             }
         }
         
-        return str.toString();
+        return sb.toString();
     }
 }
