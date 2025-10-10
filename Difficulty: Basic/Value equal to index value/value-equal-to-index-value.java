92035cbbff6008d7b1b6b4a246eeb52290ce7165
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-10-10 09:45:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Value equal to index value/value-equal-to-index-value.java
+++ Difficulty: Basic/Value equal to index value/value-equal-to-index-value.java
@@ -1,14 +1,13 @@
 // User function Template for Java
 
 class Solution {
-    public List<Integer> valueEqualToIndex(List<Integer> arr) {
+    public List<Integer> valueEqualToIndex(List<Integer> nums) {
         // code here
         List<Integer> res = new ArrayList<>();
         
-        for(int i=1; i<=arr.size(); i++){
-            if(arr.get(i-1) == i){
-                res.add(i);
-            }
+        for(int i=1; i<=nums.size(); i++){
+            int curEle = nums.get(i-1);
+            if(curEle == i) res.add(i);
         }
         
         return res;
