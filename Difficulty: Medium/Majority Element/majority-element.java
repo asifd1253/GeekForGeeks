67698f624ba96e646065179f0411582681bc76bb
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-10-10 10:39:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -1,18 +1,14 @@
 class Solution {
     int majorityElement(int arr[]) {
         // code here
-        int n = arr.length;
         HashMap<Integer, Integer> map = new HashMap<>();
         
-        for(int i=0; i<arr.length; i++){
-            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        for(int i : arr){
+            map.put(i, map.getOrDefault(i, 0)+1);
         }
         
-        for(int i=0; i<arr.length; i++){
-            int curEle = arr[i];
-            if(map.get(curEle) > n/2){
-                return arr[i];
-            }
+        for(int key : map.keySet()){
+            if(map.get(key) > (arr.length/2)) return key;
         }
         
         return -1;
