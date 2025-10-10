c7c0084d1b9da9f4dbb9956a79ea8977174cfd4b
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-10-10 10:31:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -1,27 +1,22 @@
-// User function Template for Java
-
 class Solution {
-    // Function to find two elements in array
     ArrayList<Integer> findTwoElement(int arr[]) {
         // code here
-        int n = arr.length;
         ArrayList<Integer> res = new ArrayList<>();
-        
+        int n = arr.length;
         int freq[] = new int[n+1];
-        
-        int repeated = -1;
-        int missing = -1;
         
         for(int i=0; i<n; i++){
             freq[arr[i]]++;
         }
         
-        for(int j=1; j<=n; j++){
-            if(freq[j] == 0){
-                missing = j;
-            }
-            if(freq[j] == 2){
-                repeated = j;
+        int missing = -1;
+        int repeated = -1;
+        
+        for(int i=1; i<=n; i++){
+            if(freq[i] == 0){
+                missing = i;
+            }else if(freq[i] == 2){
+                repeated = i;
             }
         }
         
