dbd0527a8a4ba74331bc84a29f5a4d5983a15f2e
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-10-24 19:35:25 +0530

    Refactor duplicate subtree detection logic

--- Difficulty: Medium/Duplicate Subtrees/duplicate-subtrees.java
+++ Difficulty: Medium/Duplicate Subtrees/duplicate-subtrees.java
@@ -1,29 +1,24 @@
 class Solution {
-    
-    String dfs(Node root, HashMap<String, ArrayList<Node>> map){
+    List<Node> res = new ArrayList<>();
+    HashMap<String, Integer> map = new HashMap<>();
+    public String dfs(Node root){
         if(root == null) return "#";
         
-        String left = dfs(root.left, map);
-        String right = dfs(root.right, map);
+        String left = dfs(root.left);
+        String right = dfs(root.right);
         
-        String subTree = left + "#" + right + "#" + root.data;
-        ArrayList<Node> ls = map.getOrDefault(subTree, new ArrayList<Node>());
-        ls.add(root);
-        map.put(subTree, ls);
+        String subTree = root.data + left + right;
+        map.put(subTree, map.getOrDefault(subTree, 0)+1);
+        
+        if(map.get(subTree) == 2){
+            res.add(root);
+        }
         
         return subTree;
     }
     public List<Node> printAllDups(Node root) {
         // code here
-        List<Node> res = new ArrayList<>();
-        HashMap<String, ArrayList<Node>> map = new HashMap<>();
-        
-        dfs(root, map);
-        
-        for(Map.Entry<String, ArrayList<Node>> entry : map.entrySet()){
-            if(entry.getValue().size() > 1) res.add(entry.getValue().get(0));
-        }
-        
+        dfs(root);
         return res;
     }
 }
