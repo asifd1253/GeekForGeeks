eb4a0365f3e8347b923bba83b49515a263ee14e3
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-11-21 06:21:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -3,23 +3,26 @@ class Solution {
         // code here
         ArrayList<Integer> arr = new ArrayList<>();
         
-        int i=0; 
-        int j=0;
+        int i=0, j=0;
         
         while(i<a.length && j<b.length){
             if(a[i] <= b[j]){
-                arr.add(a[i++]);
+                arr.add(a[i]);
+                i++;
             }else{
-                arr.add(b[j++]);
+                arr.add(b[j]);
+                j++;
             }
         }
         
         while(i<a.length){
-            arr.add(a[i++]);
+            arr.add(a[i]);
+            i++;
         }
         
         while(j<b.length){
-            arr.add(b[j++]);
+            arr.add(b[j]);
+            j++;
         }
         
         return arr.get(k-1);
