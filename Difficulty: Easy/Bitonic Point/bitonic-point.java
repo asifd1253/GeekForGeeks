81b7e6122c35692bcb2c0fc32a928e3d8aaced08
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-11-27 11:22:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bitonic Point/bitonic-point.java
+++ Difficulty: Easy/Bitonic Point/bitonic-point.java
@@ -3,21 +3,22 @@
 class Solution {
     public int findMaximum(int[] arr) {
         // code here
-        int start = 0;
-        int end = arr.length -1;
+        int st = 0;
+        int end = arr.length-1;
+        
         int ans = -1;
         
-        while(start < end){
-            int mid = start +(end - start)/2;
+        while(st < end){
+            int mid = st + (end - st)/2;
             
             if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]){
                 ans = arr[mid];
                 break;
             }else if(arr[mid] < arr[mid+1]){
                 ans = arr[mid+1];
-                start = mid+1;
+                st = mid+1;
             }else{
-                end = mid -1;
+                end = mid-1;
             }
         }
         
