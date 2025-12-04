7fc046568d65c2ba3151100a958a7107a4f45bdd
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-12-04 08:59:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.java
+++ Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.java
@@ -4,9 +4,10 @@ class Solution {
     public int findMinDiff(ArrayList<Integer> arr, int k) {
         // your code here
         Collections.sort(arr);
-        int n = arr.size();
         
         int res = Integer.MAX_VALUE;
+        
+        int n = arr.size();
         for(int i=0; i<n-k+1; i++){
             int min = arr.get(i);
             int max = arr.get(i+k-1);
