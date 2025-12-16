f2ce2569d47380bebc1dd12ade2e56c2b9d3db66
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-12-16 10:54:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.java
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.java
@@ -1,9 +1,8 @@
-// User function Template for Java
-
 class Solution {
-    int getMinDiff(int[] arr, int k) {
+    public int getMinDiff(int[] arr, int k) {
         // code here
         int n = arr.length;
+        
         Arrays.sort(arr);
         int ans = arr[n-1] - arr[0];
         
@@ -14,9 +13,7 @@ class Solution {
             int min = Math.min(smallest, arr[i+1] - k);
             int max = Math.max(largest, arr[i] + k);
             
-            if(min < 0){
-                continue;
-            }
+            if(min < 0) continue;
             
             ans = Math.min(ans, max - min);
         }
