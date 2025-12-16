eae065deb0b9014eda1daf2f5e23145a679ce105
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-12-16 11:31:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Unique Number I/unique-number-i.java
+++ Difficulty: Easy/Unique Number I/unique-number-i.java
@@ -1,19 +1,16 @@
 class Solution {
     public int findUnique(int[] arr) {
         // code here
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
         
         for(int i : arr){
-            map.put(i, map.getOrDefault(i, 0)+1);
+            if(set.contains(i)) set.remove(i);
+            else set.add(i);
         }
         
         int ans = -1;
         
-        for(int i : map.keySet()){
-            if(map.get(i) == 1){
-                ans = i;
-            }
-        }
+        for(int val : set) ans = val;
         
         return ans;
     }
