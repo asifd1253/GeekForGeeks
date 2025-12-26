a60842f1243b018d9f2e7bfb29e60556115cc318
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2025-12-26 06:36:20 +0530

    Refactor binary search method and frequency count

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -17,38 +17,42 @@ class Solution {
 
 //  Using 2nd Solution by Binary Search 
 
-// class Solution {
-//     int binarySearch(int arr[], int target, boolean searchFirst){
-//         int start = 0;
-//         int end = arr.length-1;
-//         int ans = -1;
+class Solution {
+    int binarySearch(int arr[], int target, boolean searchSideFlag){
+        int st = 0;
+        int end = arr.length-1;
         
-//         while(start <= end){
-//             int mid = start + (end - start)/2;
+        int ans = -1;
+        
+        while(st <= end){
+            int mid = st + (end-st)/2;
             
-//             if(arr[mid] == target){
-//                 ans = mid;
-//                 if(searchFirst){
-//                     end = mid-1;
-//                 }else{
-//                     start = mid+1;
-//                 }
-//             }else if(arr[mid] > target){
-//                 end = mid-1;
-//             }else{
-//                 start = mid+1;
-//             }
-//         }
+            if(arr[mid] == target){
+                ans = mid;
+                
+                if(searchSideFlag == true){ //left side if true
+                    end = mid-1;
+                }else{
+                    st = mid+1;
+                }
+            }else if(arr[mid] < target){
+                st = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
         
-//         return ans;
-//     }
-//     int countFreq(int[] arr, int target) {
-//         int first = binarySearch(arr, target, true);
-//         int last = binarySearch(arr, target, false);
+        return ans;
+    }
+    int countFreq(int[] arr, int target) {
+        // code here
+        int firstOccur = binarySearch(arr, target, true);
+        int lastOccur = binarySearch(arr, target, false);
         
-//         if(first == -1 || last == -1){
-//             return 0;
-//         }
-//         return last - first +1;
-//     }
-// }
+        if(firstOccur == -1 || lastOccur == -1){
+            return 0;
+        }
+        
+        return lastOccur - firstOccur +1;
+    }
+}
