668e7898c40dc4dd4cd47a4cd1716146b6d82ca1
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-03 12:38:09 +0530

    Fix index calculation in reverseInGroups method

--- Difficulty: Basic/Reverse array in groups/reverse-array-in-groups.java
+++ Difficulty: Basic/Reverse array in groups/reverse-array-in-groups.java
@@ -3,9 +3,9 @@ class Solution {
         // code here
         int n = arr.length;
         
-        for(int i=0; i<n; i=i+k){
+        for(int i=0; i<arr.length; i=i+k){
             int left = i;
-            int right = Math.min(i+(k-1), n-1);
+            int right = Math.min((i-1)+k, n-1);
             
             while(left < right){
                 int temp = arr[left];
@@ -16,5 +16,6 @@ class Solution {
                 right--;
             }
         }
+        
     }
 }
