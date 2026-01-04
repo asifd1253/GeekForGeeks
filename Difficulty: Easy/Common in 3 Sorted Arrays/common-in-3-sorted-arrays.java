35496d751a547e9655839ae2c1a79c23e6e3c3fa
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-04 11:59:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
@@ -1,38 +1,40 @@
 // User function Template for Java
-
 class Solution {
     // Function to find common elements in three arrays.
-    public List<Integer> commonElements(List<Integer> arr1, List<Integer> arr2,
-                                        List<Integer> arr3) {
-        // Code Here
-        List<Integer> res = new ArrayList<>();
-        
-        int i=0, j=0, k=0;
-        
-        while(i < arr1.size() && j < arr2.size() && k < arr3.size()){
-            if(arr1.get(i).equals(arr2.get(j)) && arr2.get(j).equals(arr3.get(k))){
-                if(res.isEmpty() || !res.get(res.size()-1).equals(arr1.get(i))){
-                    res.add(arr1.get(i));
-                    while(i+1 < arr1.size() && arr1.get(i).equals(arr1.get(i+1))) i++;
-                    while(j+1 < arr2.size() && arr2.get(j).equals(arr2.get(j+1))) j++;
-                    while(k+1 < arr3.size() && arr3.get(k).equals(arr3.get(k+1))) k++;
-                    
-                    i++;
-                    j++;
-                    k++;
-                }
-            }else if(arr1.get(i) < arr2.get(j)){
-                i++;
-            }else if(arr2.get(j) < arr3.get(k)){
-                j++;
-            }else{
-                k++;
+    public List<Integer> commonElements(List<Integer> arr1, List<Integer> arr2, List<Integer> arr3) {
+        ArrayList<Integer> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Step 1: Add unique elements from arr1
+        for (int i = 0; i < arr1.size(); i++) {
+            if (i > 0 && arr1.get(i).equals(arr1.get(i - 1))) continue;
+            map.put(arr1.get(i), 1);
+        }
+
+        // Step 2: For arr2 elements that exist in arr1, increment count
+        for (int i = 0; i < arr2.size(); i++) {
+            if (i > 0 && arr2.get(i).equals(arr2.get(i - 1))) continue;
+            if (map.containsKey(arr2.get(i)) && map.get(arr2.get(i)) == 1) {
+                map.put(arr2.get(i), 2);
             }
         }
-        
-        if(res.isEmpty()){
-            res.add(-1);
+
+        // Step 3: For arr3 elements that exist in both arr1 & arr2, increment count
+        for (int i = 0; i < arr3.size(); i++) {
+            if (i > 0 && arr3.get(i).equals(arr3.get(i - 1))) continue;
+            if (map.containsKey(arr3.get(i)) && map.get(arr3.get(i)) == 2) {
+                map.put(arr3.get(i), 3);
+            }
         }
+
+        // Step 4: Collect elements that appear in all three arrays
+        for (int key : map.keySet()) {
+            if (map.get(key) == 3) {
+                res.add(key);
+            }
+        }
+
+        Collections.sort(res);
         return res;
     }
 }
