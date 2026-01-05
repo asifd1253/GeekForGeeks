8ef4bd412a2fe3c89c51bb50005dd4baa020a474
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-05 10:02:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Zero Sum Subarrays/zero-sum-subarrays.java
+++ Difficulty: Medium/Zero Sum Subarrays/zero-sum-subarrays.java
@@ -2,19 +2,22 @@ class Solution {
     public int findSubarray(int[] arr) {
         // code here.
         HashMap<Integer, Integer> map = new HashMap<>();
-        int count = 0;
+        
+        map.put(0, 1);
+        
         int curSum = 0;
-        map.put(0,1);
+        int counter = 0;
         
         for(int ele : arr){
-            curSum = curSum + ele;
-            if(map.containsKey(curSum)){
-                count = count + map.get(curSum);
-            }
-            map.put(curSum, map.getOrDefault(curSum, 0)+1);
+            curSum += ele;
             
+            if(map.containsKey(curSum)){
+                counter += map.get(curSum);
+            }
+            
+            map.put(curSum, map.getOrDefault(curSum, 0)+1);
         }
         
-        return count;
+        return counter;
     }
 }
