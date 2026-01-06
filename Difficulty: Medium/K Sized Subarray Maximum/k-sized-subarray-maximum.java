8817c9315c657bb84f0ff39d31014397393a1196
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-06 10:11:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -1,3 +1,4 @@
+
 class Pair implements Comparable<Pair>{
     int val;
     int idx;
@@ -9,10 +10,10 @@ class Pair implements Comparable<Pair>{
     
     @Override
     public int compareTo(Pair p2){
-        //ascending
+        //ascending order
         // return this.val - p2.val;
         
-        //descending
+        //descending order
         return p2.val - this.val;
     }
 }
@@ -21,7 +22,6 @@ class Solution {
     public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
         // code here
         int n = arr.length;
-      
         ArrayList<Integer> res = new ArrayList<>();
         
         PriorityQueue<Pair> pq = new PriorityQueue<>();
@@ -34,11 +34,11 @@ class Solution {
         res.add(pq.peek().val);
         
         for(int i=1; i<n-k+1; i++){
-            while(!pq.isEmpty() && pq.peek().idx < i){   //remove previous elements from pq
+            while(!pq.isEmpty() && pq.peek().idx < i){
                 pq.poll();
             }
             
-            pq.offer(new Pair(arr[i+k-1], i+k-1));   //add new element
+            pq.offer(new Pair(arr[i+k-1], i+k-1));
             
             res.add(pq.peek().val);
         }
