e26c84c133455d5177e410c9a254642ae6719464
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-06 09:33:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second most repeated string in a sequence/second-most-repeated-string-in-a-sequence.java
+++ Difficulty: Easy/Second most repeated string in a sequence/second-most-repeated-string-in-a-sequence.java
@@ -5,31 +5,26 @@ class Solution {
         // your code here
         HashMap<String, Integer> map = new HashMap<>();
         
-        for(String str : arr){
-            map.put(str, map.getOrDefault(str, 0)+1);
+        for(String s : arr){
+            map.put(s, map.getOrDefault(s, 0)+1);
         }
         
-        int firFreq = 0;
-        int secFreq = 0;
+        int firstMax = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
         
-        String str1 = "";
-        String str2 = "";
-        
-        for(String str : arr){
-            int curFreq = map.get(str);
-            
-            if(firFreq < curFreq){
-                secFreq = firFreq;
-                str2 = str1;
-                
-                firFreq = curFreq;
-                str1 = str;
-            }else if(firFreq > curFreq && secFreq < curFreq){
-                secFreq = curFreq;
-                str2 = str;
+        for(int curFreq : map.values()){
+            if(curFreq > firstMax){
+                secondMax = firstMax;
+                firstMax = curFreq;
+            }else if(curFreq > secondMax && curFreq < firstMax){
+                secondMax = curFreq;
             }
         }
-       
-        return str2;
+        
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() == secondMax) return entry.getKey();
+        }
+        
+        return "";
     }
 }
