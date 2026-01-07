b915abd014c67dd7fdeefe69126ec99c00a5cfb7
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-07 10:11:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.java
+++ Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.java
@@ -3,32 +3,44 @@
 class Solution {
     void rearrange(ArrayList<Integer> arr) {
         // code here
-        LinkedList<Integer> set1 = new LinkedList<>();
-        LinkedList<Integer> set2 = new LinkedList<>();
+        ArrayList<Integer> posArr = new ArrayList<>();
+        ArrayList<Integer> negArr = new ArrayList<>();
         
-        for(int i=0; i<arr.size(); i++){
-            int curEle = arr.get(i);
-            
-            if(curEle < 0){
-                set2.add(curEle);
+        for(int i : arr){
+            if(i < 0){
+                negArr.add(i);
             }else{
-                set1.add(curEle);
+                posArr.add(i);
             }
         }
         
+        int i=0;
+        int j=0;
         
-        Iterator<Integer> posIter = set1.iterator();
-        Iterator<Integer> negIter = set2.iterator();
-        for(int i=0; i<arr.size(); i++){
-            if(i%2 == 0 && posIter.hasNext()){
-                arr.set(i, posIter.next());
-            }else if(i%2 != 0 && negIter.hasNext()){
-                arr.set(i, negIter.next());
-            }else if(posIter.hasNext()){
-                arr.set(i, posIter.next());
-            }else if(negIter.hasNext()){
-                arr.set(i, negIter.next());
+        int idx = 0;
+        
+        while(i<posArr.size() && j<negArr.size()){
+            if(idx % 2 == 0){
+                arr.set(idx, posArr.get(i));
+                i++;
+            }else{
+                arr.set(idx, negArr.get(j));
+                j++;
             }
+            
+            idx++;
+            
+        }
+        
+        while(i<posArr.size()){
+            arr.set(idx, posArr.get(i));
+            idx++;
+            i++;
+        }
+        while(j<negArr.size()){
+            arr.set(idx, negArr.get(j));
+            idx++;
+            j++;
         }
     }
 }
