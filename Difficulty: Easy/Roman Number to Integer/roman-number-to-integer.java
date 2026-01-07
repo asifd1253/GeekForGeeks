e7bf26e4d221d1e0eee1569b1a317e6ab0899cd0
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-07 09:07:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.java
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.java
@@ -3,33 +3,33 @@ class Solution {
         // code here
         HashMap<Character, Integer> map = new HashMap<>();
         
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
         
-        int result = 0;
+        int res = 0;
         
         for(int i=0; i<s.length(); i++){
             
             if(i == s.length()-1){
-                result += map.get(s.charAt(i));
+                res += map.get(s.charAt(i));
                 break;
             }
             
-            int curVal = map.get(s.charAt(i));
-            int nextVal = map.get(s.charAt(i+1));
+            int v1 = map.get(s.charAt(i));
+            int v2 = map.get(s.charAt(i+1));
             
-            if(curVal >= nextVal){
-                result = result + curVal;
+            if(v1 >= v2){
+                res += v1;
             }else{
-                result = result - curVal;
+                res = res-v1;
             }
         }
         
-        return result;
+        return res;
     }
 }
