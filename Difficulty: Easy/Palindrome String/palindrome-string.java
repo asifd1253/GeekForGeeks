0baf920ab2696790b163ed492c764cacb35c6ca0
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-08 10:52:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.java
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -1,10 +1,6 @@
 class Solution {
     boolean isPalindrome(String s) {
         // code here
-        if(s.length() == 1){
-            return true;
-        }
-        
         int left = 0;
         int right = s.length()-1;
         
@@ -12,10 +8,8 @@ class Solution {
             if(s.charAt(left) == s.charAt(right)){
                 left++;
                 right--;
-                continue;
-            }else{
-                return false;
-            }
+            }else return false;
+            
         }
         
         return true;
