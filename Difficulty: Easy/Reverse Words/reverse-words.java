2b227c9cdc2adb4179c487df6f3eab69c046420f
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-14 09:41:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.java
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -3,25 +3,25 @@ class Solution {
         // Code here
         StringBuilder sb = new StringBuilder();
         
-        int startIdx = s.length()-1;
+        int st = s.length()-1;
         
-        while(startIdx >= 0){
-            while(startIdx >= 0 && s.charAt(startIdx) == '.'){
-                startIdx--;
+        while(st >= 0){
+            while(st >=0 && s.charAt(st) == '.'){
+                st--;
             }
             
-            if(startIdx < 0) break;
+            if(st < 0) break;
             
-            int endIdx = startIdx;
+            int end = st;
             
-            while(startIdx >= 0 && s.charAt(startIdx) != '.'){
-                startIdx--;
+            while(st >=0 && s.charAt(st) != '.'){
+                st--;
             }
             
             if(sb.length() == 0){
-                sb.append(s.substring(startIdx+1, endIdx+1));
+                sb.append(s.substring(st+1, end+1));
             }else{
-                sb.append('.').append(s.substring(startIdx+1, endIdx+1));
+                sb.append('.').append(s.substring(st+1, end+1));
             }
         }
         
