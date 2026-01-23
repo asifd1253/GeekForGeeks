d478533d8acfdc71ad992f8b1feeabd348d32d9f
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-23 14:14:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest distinct window/smallest-distinct-window.java
+++ Difficulty: Medium/Smallest distinct window/smallest-distinct-window.java
@@ -2,42 +2,40 @@
 
 class Solution {
     public int findSubString(String str) {
-       // Step 1: Count all unique characters
-        Set<Character> uniqueChars = new HashSet<>();
-        for (char ch : str.toCharArray()) {
-            uniqueChars.add(ch);
+        // code here
+        HashSet<Character> set = new HashSet<>();
+        
+        for(char c : str.toCharArray()){
+            set.add(c);
         }
-        int totalUnique = uniqueChars.size(); // this is our target
-
-        // Step 2: Use HashMap to track characters in the current window
-        Map<Character, Integer> windowCount = new HashMap<>();
-        int minLen = Integer.MAX_VALUE;
-
-        int left = 0;
-        int count = 0; // count of unique characters in window
-        for (int right = 0; right < str.length(); right++) {
-            char ch = str.charAt(right);
-            windowCount.put(ch, windowCount.getOrDefault(ch, 0) + 1);
-
-            // If first time this char is added to window
-            if (windowCount.get(ch) == 1) {
-                count++;
+        
+        int setSize = set.size();
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        int i=0;
+        int j=0;
+        
+        int ans = str.length();
+        
+        while(i < str.length()){
+            char c = str.charAt(i);
+            map.put(c, map.getOrDefault(c, 0)+1);
+            
+            while(map.size() == setSize){
+                ans = Math.min(ans, i-j+1);
+                char c2 = str.charAt(j);
+              
+                map.put(c2, map.get(c2)-1);
+                
+                if(map.get(c2) == 0) map.remove(c2);
+                
+                j++;
             }
-
-            // Shrink window if it contains all unique characters
-            while (count == totalUnique) {
-                minLen = Math.min(minLen, right - left + 1);
-
-                // Try to remove str[left] from window
-                char leftChar = str.charAt(left);
-                windowCount.put(leftChar, windowCount.get(leftChar) - 1);
-                if (windowCount.get(leftChar) == 0) {
-                    count--;
-                }
-                left++;
-            }
+            i++;
         }
-
-        return minLen;
+        
+        return ans;
+        
     }
 }
