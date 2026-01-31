1231645c9a4b6ade1d0165eaab45b4379c2da36d
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-31 12:09:43 +0530

    Refactor search to separate first and last occurrence methods

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -1,39 +1,56 @@
 // User function Template for Java
 
 class GFG {
-    int search(int arr[], int x, boolean whichSide){
+    int firstOccur(int arr[], int x){
         int st = 0;
-        int end = arr.length - 1;
-        int ans = -1;
+        int end = arr.length-1;
         
+        int idx = -1;
         while(st <= end){
             int mid = st + (end-st)/2;
             
             if(arr[mid] == x){
-                ans = mid;
-                if(whichSide == true){
-                    end = mid-1;
-                }else{
-                    st = mid+1;
-                }
-            }else if(arr[mid] > x){
+                idx = mid;
+                end = mid-1;
+            }else if(x < arr[mid]){
                 end = mid-1;
             }else{
                 st = mid+1;
             }
         }
         
-        return ans;
+        return idx;
+    }
+    
+    int lastOccur(int arr[], int x){
+        int st = 0;
+        int end = arr.length-1;
+        
+        int idx = -1;
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            
+            if(arr[mid] == x){
+                idx = mid;
+                st = mid+1;
+            }else if(x < arr[mid]){
+                end = mid-1;
+            }else{
+                st = mid+1;
+            }
+        }
+        
+        return idx;
     }
     ArrayList<Integer> find(int arr[], int x) {
         // code here
+        int firstIdx = firstOccur(arr, x);  //firstOccurence
+        int lastIdx = lastOccur(arr, x);  //lastOccurence
+        
         ArrayList<Integer> res = new ArrayList<>();
         
-        int firstOccur = search(arr, x, true);
-        int lastOccur = search(arr, x, false);
-        
-        res.add(firstOccur);
-        res.add(lastOccur);
+        res.add(firstIdx);
+        res.add(lastIdx);
         
         return res;
     }
