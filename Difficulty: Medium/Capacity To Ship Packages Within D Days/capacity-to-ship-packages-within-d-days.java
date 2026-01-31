fd477f6ba12acf6ace1023e31795e4ec9d43748d
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-01-31 15:23:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Capacity To Ship Packages Within D Days/capacity-to-ship-packages-within-d-days.java
+++ Difficulty: Medium/Capacity To Ship Packages Within D Days/capacity-to-ship-packages-within-d-days.java
@@ -1,46 +1,46 @@
 // User function Template for Java
 
 class Solution {
-    static boolean isValidCapacity(int weights[], int mid, int days){
+    static boolean isValidCap(int arr[], int maxAllowCap, int totDays){
         int noOfDays = 1;
-        int sumOfWeights = 0;
+        int curCap=0;
         
-        for(int weight : weights){
-            //Edge case
-            if(weight > mid){
-                return false;
-            }
+        for(int weight : arr){
+            if(weight > maxAllowCap) return false;
             
-            if(weight + sumOfWeights > mid){
-                noOfDays++;
-                sumOfWeights = weight;
+            if(curCap+weight <= maxAllowCap){
+                curCap += weight;
             }else{
-                sumOfWeights += weight;
+                noOfDays++;
+                curCap = weight;
             }
         }
         
-        return (noOfDays <= days)?true:false;
+        return (noOfDays<=totDays)?true:false;
     }
-    
-    static int leastWeightCapacity(int[] weights, int n, int days) {
+    static int leastWeightCapacity(int[] arr, int n, int d) {
         // code here
-        int minCap = 0;
+        int minCap = Integer.MAX_VALUE;
         int maxCap = 0;
         
-        for(int weight : weights){
-            minCap = Math.max(minCap, weight);
-            maxCap += weight;
+        for(int w : arr){
+            minCap = Math.min(minCap, w);
+            maxCap = maxCap+w;
         }
         
+        int st = minCap;
+        int end = maxCap;
+        
         int ans = -1;
-        while(minCap <= maxCap){
-            int mid = minCap + (maxCap - minCap)/2;
+        
+        while(st <= end){
+            int mid = st +(end-st)/2;
             
-            if(isValidCapacity(weights, mid, days)){
+            if(isValidCap(arr, mid, d)){
                 ans = mid;
-                maxCap = mid - 1;
+                end = mid-1;
             }else{
-                minCap = mid + 1;
+                st = mid+1;
             }
         }
         
