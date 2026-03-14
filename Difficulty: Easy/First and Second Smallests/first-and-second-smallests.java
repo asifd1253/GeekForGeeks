01e464b2b379497f8fbbfd51d95af699c910a6ab
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-03-14 12:28:02 +0530

    Optimize minAnd2ndMin to find min and second min
    Refactored minAnd2ndMin method to find the first and second smallest numbers in a single pass without sorting.

--- Difficulty: Easy/First and Second Smallests/first-and-second-smallests.java
+++ Difficulty: Easy/First and Second Smallests/first-and-second-smallests.java
@@ -1,22 +1,47 @@
 class Solution {
     public ArrayList<Integer> minAnd2ndMin(int[] arr) {
         // code here
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
+        
+        // ArrayList<Integer> res = new ArrayList<>();
+        
+        // for(int i : arr){
+        //     if(!res.contains(i) && res.size() < 2){
+        //         res.add(i);
+        //     }
+        // }
+        
+        // if(res.size() == 1){
+        //     res.set(0, -1);
+        // }
+        
+        // return res;
         
         ArrayList<Integer> res = new ArrayList<>();
         
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int i : arr){
-            if(!set.contains(i) && res.size() < 2){
-                res.add(i);
-            }
-            
-            set.add(i);
+        if (arr.length < 2) {
+            res.add(-1);
+            return res;
         }
         
-        if(res.size() == 1){
-            res.set(0, -1);
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+        
+        // Find min and second min in single pass
+        for (int num : arr) {
+            if (num < min) {
+                secondMin = min;
+                min = num;
+            } else if (num < secondMin && num != min) {
+                secondMin = num;
+            }
+        }
+        
+        if (secondMin == Integer.MAX_VALUE) {
+            res.add(-1);
+        } else {
+            res.add(min);
+            res.add(secondMin);
         }
         
         return res;
