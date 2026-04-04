fefe75be3e090f5d41c293bb83352130b860f7dc
Dunnapothula Asif <140490107+asifd1253@users.noreply.github.com>
2026-04-04 19:15:33 +0530

    Refactor sorting by frequency using PriorityQueue

--- Difficulty: Medium/Sort Elements by Decreasing Frequency/sort-elements-by-decreasing-frequency.java
+++ Difficulty: Medium/Sort Elements by Decreasing Frequency/sort-elements-by-decreasing-frequency.java
@@ -6,26 +6,37 @@ class Solution {
     // Function to sort the array according to frequency of elements.
     public ArrayList<Integer> sortByFreq(int arr[]) {
         // add your code here
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> res = new ArrayList<>();
         
         HashMap<Integer, Integer> map = new HashMap<>();
         
         for(int i : arr){
             map.put(i, map.getOrDefault(i, 0)+1);
-            list.add(i);
         }
         
-        Collections.sort(list, (a,b)->{
-            int freqA = map.get(a);
-            int freqB = map.get(b);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            if(a[1] != b[1]){
+                return b[1] - a[1];
+            }
             
-            if(freqA != freqB) return freqB - freqA;
-            
-            return a-b;
+            return a[0] - b[0];
         });
         
-        return list;
+        map.forEach((key, val)->{
+            pq.offer(new int[]{key, val});
+        });
         
-      
+        while(!pq.isEmpty()){
+            int cur[] = pq.poll();
+            
+            int curEle = cur[0];
+            int curFreq = cur[1];
+            
+            for(int i=0; i<curFreq; i++){
+                res.add(curEle);
+            }
+        }
+        
+        return res;
     }
 }
